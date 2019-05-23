83b7b875a7748af9870070ea96289c5b3a9afc07
lhsharing <daley.luis@gmail.com>
2019-05-23 16:10:22 -0400

    +destroy engineer in application_ctrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+#  def destroy
+# #    session[:engineer_id] = nil
+#  session[:current_user_id] = user.id
+# sign_out @engineer 
+# redirect_to root_url, notice: "Logged out!"
+#   end
+
+def destroy
+    Engineer.find(params[:id]).destroy
+    redirect_to action: 'index'
+    #redirect_to Engineer_path
+  end
+
+
+
 end
